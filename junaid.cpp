51c90df4900a90d192c21dd6e157057b1672028e
Junaid Khan <jk7466809@gmail.com>
2025-06-12 16:19:00 +0530

    Initial Commit

---
+++ junaid.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class abc{
+    public:
+    int sum(int x){
+        if(x == 1){
+            return 1;
+        }
+        return x + sum(x-1);
+    }
+};
+
+int main(){
+    int n;
+    int add;
+    abc ob;
+
+    cout<<"the sum of first n natural numbers :" << endl;
+    cout<<"Enter the value of n :" << endl;
+
+    cin >> n;
+    add = ob.sum(n);
+
+    cout<<"The sum of all the numbers from 1 to " << n << " is : " << add << endl;
+}
